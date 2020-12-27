8e7df426b98248e7a256543e71bac53a94ab3eef
OleStan <59766354+OleStan@users.noreply.github.com>
2020-12-27 19:13:17 +0200

    add autorisation last and dependense in post redactions

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,8 @@
 class Author < ApplicationRecord
   has_secure_password
+  
+  VALID_EMAIL_REGX= /^[\w\d]+@[\w\d]+\.[\w]+/
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
   def full_name
     "#{self.first_name} #{self.last_name}"
