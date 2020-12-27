57aef47acbc4e5e173ab680afd90704b8aeca93d
OleStan <59766354+OleStan@users.noreply.github.com>
2020-12-27 22:04:47 +0200

    add autorisation last and post adition

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,9 +1,10 @@
 class Author < ApplicationRecord
   has_secure_password
-  
-  VALID_EMAIL_REGX= /^[\w\d]+@[\w\d]+\.[\w]+/
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
+  VALID_EMAIL_REGX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :first_name, :last_name, :gender, :birthday, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGX }
+  validates_uniqueness_of :email, case_sensitive: false
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
